08c4f0e781ebb8e2f4262d10ebc5d14df1e9c291
Ravi Jyoti <ravijyoti3@gmail.com>
2022-09-07 11:29:25 +0530

    Added ability to display list of tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  # respond_to :html, :xml, :json
-
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: tasks }
-
-    # respond_with(@tasks)
   end
 end
