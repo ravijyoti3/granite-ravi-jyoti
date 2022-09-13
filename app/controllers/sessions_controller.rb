7111df5894eb8b4704f420082df2f391280ae854
Ravi Jyoti <ravijyoti3@gmail.com>
2022-09-13 13:06:52 +0530

    Added ability to logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,11 @@ class SessionsController < ApplicationController
     unless @user.authenticate(login_params[:password])
       respond_with_error(t("session.incorrect_credentials"), :unauthorized)
     end
+
+    def destroy
+      @current_user = nil
+      # any other session cleanup tasks can be done here...
+    end
   end
 
   private
