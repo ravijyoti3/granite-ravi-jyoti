d6e6c6277e4de10eb658158b552e9e8964df064e
Ravi Jyoti <ravijyoti3@gmail.com>
2022-09-14 14:11:02 +0530

    Fixed order of items in Active Record models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,36 +2,30 @@
 
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  MAX_EMAIL_LENGTH = 255
-  MAX_NAME_LENGTH = 35
 
   has_many :comments, dependent: :destroy
-  has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
-  has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
-  has_secure_password
-  has_secure_token :authentication_token
+  has_many :tasks, dependent: :destroy, foreign_key: :user_id
 
-  validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
-  validates :email, presence: true,
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email,
+    presence: true,
     uniqueness: { case_sensitive: false },
-    length: { maximum: MAX_EMAIL_LENGTH },
+    length: { maximum: 50 },
     format: { with: VALID_EMAIL_REGEX }
-  validates :password, length: { minimum: 6 }, if: -> { password.present? }
+  validates :password,
+    presence: true,
+    confirmation: true,
+    length: { minimum: 6 }
   validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lowercase
-  before_destroy :assign_tasks_to_task_owners
+
+  has_secure_password
+  has_secure_token :authentication_token
 
   private
 
     def to_lowercase
       email.downcase!
-    end
-
-    def assign_tasks_to_task_owners
-      tasks_whose_owner_is_not_current_user = assigned_tasks.select { |task| task.task_owner_id != id }
-      tasks_whose_owner_is_not_current_user.each do |task|
-        task.update(assigned_user_id: task.task_owner_id)
-      end
     end
 end
